36527b25f4e9920e1e1e38a378ce54ec047e6049
Sebastien de Larquier <sdelarquier@vt.edu>
2012-09-26 11:54:40 -0400

    added igrf example to AJs demo

---
+++ igrf/__init__.py
@@ -0,0 +1,13 @@
+# IGRF module __init__.py
+"""
+*******************************
+            IGRF
+*******************************
+This subpackage contains IGRF11
+
+All subroutines for the IGRF fortran file are available in Python.
+
+*******************************
+"""
+
+from igrf import *
